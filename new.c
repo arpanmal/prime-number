6e9ff01686e2168393ece06efe6bef4cafc0ac22
Arpan <41269609+arpanmal@users.noreply.github.com>
2021-09-09 23:26:19 +0530

    Add files via upload

---
+++ new.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+int number =71;
+int div=71/2;
+for (int i=2;i<=div;i++){
+    if(number% i==0){
+printf("given number is not a prime no");
+break;}}
+printf("given number is a prime no");
+
+}
